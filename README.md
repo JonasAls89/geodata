05e562a40427b1588d8f8bfc277f5f4ec2ae722c
Jonas Als Christensen <jonas.christensen@M-C02TP0J9HF1T.local>
2019-08-08 09:49:29 +0200

    Adding working repo with args input for wkid

---
+++ README.md
@@ -0,0 +1,23 @@
+# Geodata-Connector
+
+## This is a repo for connecting to geodata.no
+
+## To install requirements run the following
+
+```
+yarn install
+```
+
+## In root - create a auth_headers.yml file to contain username, password and referrer.
+
+i.e.
+
+```
+username : foo
+password : bar
+referrer : arcgis.mydomain.com
+```
+
+## To run the app :
+in package.json you'll find the
+commands needed to run the app.
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import json
+import requests
+import yaml
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    output = {
+        'service': 'Geodata.no Connector',
+        'remote_addr': request.remote_addr
+    }
+    return jsonify(output)
+
+@app.route('/get_geo', methods=['GET'])
+def get_data():
+    wkid = request.args['wkid']
+    ## Generating token
+    stream = open('auth_headers.yml', 'r')
+    settings = yaml.load(stream, yaml.SafeLoader)
+    headers = {
+        'username': "{0}".format(settings['username']),
+        'password': "{0}".format(settings['password']),
+        'referrer': "{0}".format(settings['referrer'])
+    }
+    genereateToken = "https://services.geodataonline.no/arcgis/tokens/generateToken/query?username=%s&password=%s&referer=%s&f=pjson" % (headers['username'], headers['password'], headers['referrer'])
+    headerToken = requests.get(genereateToken).json()
+    token = {'Authorization' : 'Bearer ' + headerToken['token']}
+    
+    ## Requesting geo data
+    request_url = "https://services.geodataonline.no/arcgis/rest/services/Geomap_UTM33_EUREF89/GeomapMatrikkel/MapServer/5/query?geometry=%7B%22x%22%3A485817%2C%22y%22%3A6478206%2C%22spatialReference%22%3A%7B%22wkid%22%3A25832%7D%7D&geometryType=esriGeometryPoint&inSR=25832&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=kommunenr%2Cgardsnr%2Cbruksnr&returnGeometry=false&returnTrueCurves=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=false&returnDistinctValues=false&f=pjson"
+    test = requests.get(request_url, headers=token)
+    return jsonify(test.json())
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=5000, debug=True, threaded=True)
